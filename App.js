8426373ed1d0d4c6d72e32f7cb5f06c47df8acce
Murat Akdağ <75887798+muratakdag@users.noreply.github.com>
2022-02-28 10:34:47 +0300

    Final Version of the App

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import { useScreens } from 'react-native-screens';
 
-import productsReducer from './store/reducers/products';
-import ShopNavigator from './navigation/ShopNavigator';
+import MealsNavigator from './navigation/MealsNavigator';
 
-const rootReducer = combineReducers({
-  products: productsReducer
-});
+useScreens();
 
-const store = createStore(rootReducer);
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <ShopNavigator />
-    </Provider>
-  );
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
+  return <MealsNavigator />;
 }
