f87e26d6621790f7ac619f0ec09ea73bfd3dc09f
Murat Akdağ <75887798+muratakdag@users.noreply.github.com>
2022-02-28 10:30:19 +0300

    Add Touchable Components

--- App.js
+++ App.js
@@ -1,31 +1,20 @@
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-import { useScreens } from 'react-native-screens';
+import React from 'react';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-import MealsNavigator from './navigation/MealsNavigator';
+import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
-useScreens();
+const rootReducer = combineReducers({
+  products: productsReducer
+});
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-};
+const store = createStore(rootReducer);
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
-  if (!fontLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setFontLoaded(true)}
-      />
-    );
-  }
-
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
+  );
 }
